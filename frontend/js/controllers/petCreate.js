cabb4b654069852ecc5e546058fee6d2cefa9b2f
ivey <iveysaurrr@gmail.com>
2015-03-21 12:02:03 -0400

    Creating pet SHOULD handle success and failure

--- frontend/js/controllers/petCreate.js
+++ frontend/js/controllers/petCreate.js
@@ -2,6 +2,7 @@ PetsApp.controller("petCreateController", function($scope) {
 	$scope.pets = Pets;
 	$scope.chosen = null
 	$scope.form = {};
+	$scope.error = "";
 
 	$scope.choosePet = function (index) {
 		$scope.form = {};
@@ -12,7 +13,11 @@ PetsApp.controller("petCreateController", function($scope) {
 		$scope.form.id = $scope.chosen.id;
 		$scope.form.userId = document.cookie.split(',')[0].substr(2);
 		post('/api/pet/create', $scope.form, function(data) {
-			console.log(data);
+			if (data.status = true) {
+				location.href = "#/"
+			} else {
+				$scope.error = "Something went wrong.";
+			}
 		})
 	}
 });
