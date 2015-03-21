de569527eea6ad6a095c4a9999475a6db0bdfd82
ivey <iveysaurrr@gmail.com>
2015-03-21 09:14:18 -0400

    Add post function to get pet create working

--- frontend/js/controllers/petCreate.js
+++ frontend/js/controllers/petCreate.js
@@ -1,22 +1,18 @@
-PetsApp.controller("petCreateController", function($scope, $routeParams, $http) {
+PetsApp.controller("petCreateController", function($scope) {
 	$scope.pets = Pets;
 	$scope.chosen = null
 	$scope.form = {};
 
-	console.log($scope.pets);
-
 	$scope.choosePet = function (index) {
 		$scope.form = {};
-		console.log($scope.pets[index]);
 		$scope.chosen = $scope.pets[index];
 	}
 
 	$scope.createPet = function() {
 		$scope.form.id = $scope.chosen.id;
 		$scope.form.userId = document.cookie.split(',')[0].substr(2);
-		console.log($scope.form);
-		$http.post('/api/pet/create', $scope.form).success(function(data) {
+		post('/api/pet/create', $scope.form, function(data) {
 			console.log(data);
-		});
+		})
 	}
 });
