c9d61a528fd88f883e97c95daeb0be1f2c439e0e
Jonathan Warner <jaxbot@gmail.com>
2015-03-21 01:10:00 -0400

    Some initial work on NPC system

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,6 +3,10 @@ PetsApp.config(function($routeProvider) {
         $routeProvider.when('/', {
                 templateUrl: 'templates/main.html',
                 controller: 'mainController'
+        })
+        .when('/npc/:id', {
+                templateUrl: 'templates/npc.html',
+                controller: 'npcController'
         });
 });
 PetsApp.controller('siteController', function() {
